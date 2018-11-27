1c1f34a6fdaacde8875dbaee1fdad0e64157a5fd
Luc AUCOIN <luc.aucoin@viacesi.fr>
2018-11-27 16:49:56 +0100

    correct bug AOSONG

--- sketch_nov20a/sketch_nov20a.ino
+++ sketch_nov20a/sketch_nov20a.ino
@@ -2,32 +2,70 @@
 #include <DHT.h>
 #include <math.h>
 
+#define DHTPIN 2     // what pin we're connected to
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+
 long Resistance ; 
 int analogPin = 0;     // potentiometer wiper (middle terminal) connected to analog pin 3
                        // outside leads to ground and +5V
 int val = 0;           // variable to store the value read
 
-double Thermistor(int RawADC) {
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup()
+{
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop()
+{
+  Serial.print("Humidity (carre blanc) : ");
+  Serial.print(getUpHumidity()); //display humidity
+  Serial.println("%");
+  
+  Serial.print("Temperature (carre blanc) : ");
+  Serial.print(getUpTemp()); //display humidity
+  Serial.println("°C");
+  
+  Serial.print("Temperature: ");
+  Serial.print(float(getBotTemp(analogRead(ThermistorPIN))));  // display Celsius
+  Serial.println("°C\n");
+  
+  delay(2000);
+}
+
+double getUpHumidity()
+{
+  // Reading humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float humidity = dht.readHumidity();
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(humidity))
+    Serial.println("Failed to read humidity from DHT sensor!");
+
+  return humidity;
+}
+
+double getUpTemp()
+{
+  // Reading temperature takes about 250 milliseconds!
+  // Read temperature as Celsius
+  float temperature = dht.readTemperature();
+  
+  if(isnan(temperature))
+    Serial.println("Failed to read temperature from DHT sensor!");
+
+  return temperature;
+}
+
+double getBotTemp(int RawADC)
+{
   double temp;
   Resistance = 10000.0/(1023.0/RawADC - 1);
   temp = log(Resistance); //Finds the log of Resistance
   temp = 1 / (0.001125294 + (0.000234715 + (0.0000000856502 * temp * temp ))* temp); //Transforms temp to Kelvin
   temp = temp - 273.15; //Transforms temp to Celsius
   return temp;
- };
-
-
-
-void setup()
-{
-  Serial.begin(115200);
-}
-
-void loop()
-{
-  Serial.print("Humidity: ");
-  Serial.println(analogRead(5)); //display humidity
-  Serial.print("Temperature: ");
-  Serial.println(int(Thermistor(analogRead(ThermistorPIN))));  // display Celsius
-  delay(100);
 }
